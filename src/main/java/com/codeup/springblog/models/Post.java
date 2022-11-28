e3fa85dcb95cc61e17d6d04121df1734efe4b829
Daniel Name <daniel.v.name@gmail.com>
2022-11-28 15:16:21 -0600

    refactored posts.java to make it neat and organized

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -4,52 +4,55 @@ import javax.persistence.*;
 @Entity
 @Table(name="posts")
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column(nullable = false, length = 100)
-    private String title;
-    @Column(nullable = false, columnDefinition = "LONGTEXT")
-    private String body;
-    @ManyToOne
-//    @JoinColumn(name = "user_id")  <--this is useful for when we are using the <TABLE_NAME>_id naming convention
-    private User user;
-
-    public Post() {
-    }
-
-    public Post(String title, String body) {
-        this.title = title;
-        this.body = body;
-    }
     public Post(long id, String title, String body) {
         this.id = id;
         this.title = title;
         this.body = body;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Post(String title, String body) {
         this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
         this.body = body;
     }
 
+    public Post() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String body;
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+    @ManyToOne
+//    @JoinColumn(name = "user_id")  <--this is useful for when we are using the <TABLE_NAME>_id naming convention
+    private User user;
 }
