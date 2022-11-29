449be3efe8dcffb206caf28b2e3705fe09a81143
Daniel Name <daniel.v.name@gmail.com>
2022-11-29 09:15:58 -0600

    clean commit

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -4,6 +4,14 @@ import javax.persistence.*;
 @Entity
 @Table(name="posts")
 public class Post {
+
+    public Post(long id, String title, String body, User user) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
     public Post(long id, String title, String body) {
         this.id = id;
         this.title = title;
