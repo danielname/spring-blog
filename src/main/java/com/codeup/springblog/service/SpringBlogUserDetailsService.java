33714427795f5cb46d81e7e264a251044ae922dc
Daniel Name <daniel.v.name@gmail.com>
2022-11-30 15:16:38 -0600

    user details service complete

--- src/main/java/com/codeup/springblog/service/SpringBlogUserDetailsService.java
+++ src/main/java/com/codeup/springblog/service/SpringBlogUserDetailsService.java
@@ -1,12 +1,28 @@
 package com.codeup.springblog.service;
 
+import com.codeup.springblog.models.SpringBlogUserDetails;
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpringBlogUserDetailsService implements UserDetailsService {
+    public final UserRepository userDao;
+
+    public SpringBlogUserDetailsService(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User details not found for " + username);
+        } else {
+            return new SpringBlogUserDetails(user.getId(), user.getUsername(), user.getEmail(), user.getPassword());
+        }
     }
 }
