326aad3bd997f5ac59fef4ed6352e0bbe2c99b77
Daniel Name <daniel.v.name@gmail.com>
2022-12-01 10:00:48 -0600

    added active user id to create

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,9 +1,11 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
 //import com.codeup.springblog.service.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -45,6 +47,8 @@ public class PostController {
 
     @GetMapping("/create")
     public String postForm(Model model){
+        User activeUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("activeUserId", activeUser.getId());
         model.addAttribute("post", new Post());
         return "/posts/create";
     }
