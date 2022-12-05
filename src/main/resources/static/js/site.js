24e8738e1f62296e2965f3998c8bbfe13074b555
Daniel Name <daniel.v.name@gmail.com>
2022-12-05 13:57:05 -0600

    fixed nav

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,20 +1,29 @@
 $(function (){
-alert("yo");
-
-    /* When the user clicks on the button,
-    toggle between hiding and showing the dropdown content */
-
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-    }
-}
+// const nav = {
+//     toggleNav(){
+//         $('.dropdown-content').toggleClass('show');
+//     },
+//     closeNav(){
+//
+//     }
+// }
+//
+// $('.dropbtn').on('click',nav.toggleNav());
+//
+//     /* When the user clicks on the button,
+//     toggle between hiding and showing the dropdown content */
+//
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+//
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//         var myDropdown = document.getElementById("myDropdown");
+//         if (myDropdown.classList.contains('show')) {
+//             myDropdown.classList.remove('show');
+//         }
+//     }
+// }
 });
